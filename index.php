56413ddc2b4fc2fc802bb7e263d4e9ad79b44eb8
Alvar <alvarsnow@gmail.com>
2018-01-30 18:32:43 +0100

    inicio

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
-
 	<?php
 		include "header.php"; 
 		include "lib.php";
-		test();
+
+		session_start();
+		$cnonexion = start_conection("localhost","root","","videoclub");
+
+		if ( isset($_POST['intro'])) {
+
+		} else {
+
+		}
 	?>
+
 	<form action="" method="post">
-		<p>Usuario: </p>
+		<p>ID Usuario: </p>
 		<input type="text" name="usuario">
 		<p>Contraseña: </p>
 		<input type="password" name="pass"><br>
 		<input type="checkbox" name="empleado"> Empleado <br>
 		<input type="submit" name="intro" value="Intro">
+		<input type="submit" name="reg" value="Registro">
 	</form>
-</body>
-</html>
+
+<?php include "footer.php" ?>
--- lib.php
+++ lib.php
@@ -1,5 +1,25 @@
 <?php
-	function test(){
-		echo "TEST";
+	function autenticar($cnx,$empleado,$id,$pass){
+		if ($empleado) {
+			$q = "SELECT * FROM empleado WHERE id_usuario = $id;";
+		}	else {
+			$q = "SELECT * FROM socio WHERE id_usuario = $id;";
+		}
+
+		if($resultado = $cnx -> query($q)){
+			
+		}
+
+		
+	}
+
+	function start_conection($ip,$usuario,$pass,$bdd){
+		$mysqli = new mysqli($ip, $usuario, $pass, $bdd);
+
+		if ($mysqli->connect_errno) {
+    	echo "Falló la conexión con MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+		}
+
+		return $mysqli;
 	}
 ?>
