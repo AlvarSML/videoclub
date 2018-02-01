8cdc1f4380749b7cb34b3e2b3abf1c42485e9b01
Alvar <alvarsnow@gmail.com>
2018-02-01 19:48:50 +0100

    menu empleado

---
+++ nDisco.php
@@ -0,0 +1,5 @@
+<form>
+  Nombre: <input type="text" name="nombre"><br>
+  Autor: <input type="text" name="autor">
+  <input type="submit" name="ndisco" value="Intro">
+</form>
---
+++ nuevo.php
@@ -0,0 +1,16 @@
+<?php
+include "header.php"; 
+include "lib.php";
+session_start();
+
+if (isset($_POST['disc'])) {
+  
+} else if (isset($_POST['disc'])) {
+
+} else {
+  
+}
+
+?>
+
+<?php  include "footer.php" ?>
