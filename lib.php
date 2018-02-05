b9c7052506037322cf840831cb2365495bc08d16
Alvar <alvarsnow@gmail.com>
2018-02-05 19:36:27 +0100

    Funcion vertTabla
    añadida la funcion verTabla y ajustadas las de añadir discos/videos

--- lib.php
+++ lib.php
@@ -1,4 +1,6 @@
 <?php
+/** TODO: Cerrar las conexiones!!!! */
+
 function formularioInicial(){
 ?>
 <form action="index.php" method="post">
@@ -54,11 +56,11 @@ function nuevoUsuario( $nombre, $clave, $apellidos) {
 }
 
 /**
-	 * Comprueba un usuario y contraseña
-	 * @param empleado Boolean - si es empleado o no
-	 * @param id Integer - Id del usuario
-	 * @param pass String - Contraseña del usuario
-	 */
+	* Comprueba un usuario y contraseña
+	* @param empleado Boolean - si es empleado o no
+	* @param id Integer - Id del usuario
+	* @param pass String - Contraseña del usuario
+*/
 function autenticar($nombre,$pass){
     $cnx = start_conection("localhost","root","","videoclub");
     $q = "SELECT * FROM usuario WHERE nombre = '$nombre';";
@@ -80,12 +82,105 @@ function autenticar($nombre,$pass){
     }
     $cnx -> close();
 }
+
+/**
+ * Comienza y devuleve una conexion
+ * @param ip - IP o direccion de la base de datos
+ * @param usaurio - usuario
+ * @param pass - contraseña
+ * @param bdd - nombre de la base de datos
+ */
 function start_conection($ip,$usuario,$pass,$bdd){
-    $mysqli = new mysqli($ip, $usuario, $pass, $bdd);
+    $mysqli = new mysqli("localhost","root","","videoclub");
     if ($mysqli->connect_errno) {
         echo "Falló la conexión con MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
     return $mysqli;
 }
 
+/**
+ * crea un registro de video
+ * @param nombre
+ * @param prota 
+ */
+function introVideo($nombre,$prota){
+    $conexion = start_conection("localhost","root","","videoclub");
+
+    $insert = "INSERT INTO video VALUES (0,NULL'$nombre','$prota');";
+
+    if($conexion->query($insert)){
+        echo "Inser correcto";
+    } else {
+        echo "Error de insert";
+    }
+
+    $conexion -> close();
+    
+}
+
+/**
+ * crea un registro de video
+ * @param nombre
+ * @param autor 
+ */
+function introDisco($nombre,$autor){
+
+    $conexion = start_conection("localhost","root","","videoclub");
+
+    $insert = "INSERT INTO disco VALUES (0,NULL,'$nombre','$autor');";
+
+    if($conexion->query($insert)){
+        echo "Inser correcto";
+    } else {
+        echo "Error de insert";
+        echo $insert;
+    }
+
+    $conexion -> close();
+    
+}
+
+/**
+ * Crea una tabla con los datos de una consulta
+ * 
+ * @param string $tabla - tabla de la query
+ * @param string[] $campos - campos a sacar
+ * @return HTMLtabla - con datos del select
+ */
+function verTabla($tabla,$campos){
+    $conexion = start_conection("localhost","root","","videoclub");
+
+    $titulo = "";
+    $filas = "";
+
+    /**
+     * @var cam = $campos (array) pasado a cadena 
+     */
+    $cam = implode(',',$campos);
+    
+    for ($i = 0; $i <count($campos); $i++) {
+        $titulo .= "<th>".$campos[$i]."</th>";
+    }   
+    $titulos = "<tr>$titulo</tr>";
+
+    $q = "SELECT $cam FROM $tabla;";
+    //echo $q;
+
+    if($res = $conexion -> query($q)){
+        //echo "Select en $tabla completado";
+        
+        while ($row = $res -> fetch_array(MYSQLI_NUM)) {
+            $filas .= "<tr>";
+            for ($i = 0; $i <= count($row) -1; $i ++) {
+                $filas .= "<td>$row[$i]</td>";
+            }
+            $filas .= "</td>";
+        }
+
+    } else {
+        echo "Select en $tabla fallido";
+    }
+
+    return "<table border='1'>$titulos $filas</table>";
+}
 ?>
