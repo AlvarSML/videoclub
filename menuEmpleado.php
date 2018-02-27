fac93e957adcf3195808d031ccd54e7817fad134
Alvar <alvarsnow@gmail.com>
2018-02-27 23:06:58 +0100

    aluiler

--- menuEmpleado.php
+++ menuEmpleado.php
@@ -7,7 +7,7 @@
   <input type="submit" name="disc" value="Gestionar discos">
 
   <input type="submit" name="dvd" value="Gestionar videos">
- 
+  
   <?php
     }
 
@@ -17,6 +17,18 @@
   ?>
   <input type="submit" name="exit" value="Cerrar sesion">
 </form>
+
+<?php
+  if (getPriv($_SESSION['user']) > 2) { 
+?>
+<form action="alquiler.php" method="post">
+<input type="submit" name="alquiler" value="Alquiler">
+</form>
+
+<?php
+}
+?>
+
 <form action="gestionUsuario.php" method="post">
   <input type="submit" name="actAlq" value="Alquileres actuales">
   <input type="submit" name="histAlq" value="Historial de alquileres">
