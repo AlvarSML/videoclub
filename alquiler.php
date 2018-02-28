a4990efd4ded40bcc70c16e1a61e23edeab3c486
Alvar <alvarsnow@gmail.com>
2018-02-28 20:31:54 +0100

    asd
    mod alquiler

--- alquiler.php
+++ alquiler.php
@@ -3,25 +3,24 @@ include "lib.php";
 print('<form action="nuevo.php" method="post">');
 $conexion = start_conection("localhost","root","","videoclub");
 print('<h1>SOCIO</h1>');
-$sql1 = "select * from usuario where permiso=1";
-$resultado1 = $conexion->query($sql1);
+
+$sql1 = "SELECT * FROM usuario WHERE permiso = 1";
+$resultado1 = $conexion->query($sql1) or die ($sql1);
+
 print('Nombre del socio:');
 print('<select name="socios">');
 for ($i = 0; $i<$resultado1->num_rows;$i++){
     $row = $resultado1->fetch_assoc();
     $idUsuario = $row['id_usuario'];
     $nombreSocio = $row['nombre'];
-    $sql = "select * from socio where id_usuario=$idUsuario";
-    $resultado = $conexion->query($sql);
-    $roww = $resultado->fetch_assoc();
-    $idSocio = $roww['id_socio'];
-    print('<option value="'.$idSocio.'">'.$nombreSocio.'');
+
+    print('<option value="'.$idUsuario.'">'.$nombreSocio.'');
     print('</option>');
 }
 print('</select>');
 print('<h1>DISCOS</h1>');
 $sql2 = "select * from disco where id_prestamo IS NULL";
-$resultado2 = $conexion->query($sql2);
+$resultado2 = $conexion->query($sql2) or die ($sql2);
 print('Nombre del disco:');
 print('<select name="discos">');
 for ($i = 0; $i<$resultado2->num_rows;$i++){
@@ -34,7 +33,7 @@ for ($i = 0; $i<$resultado2->num_rows;$i++){
 print('</select>');
 print('<h1>VIDEOS</h1>');
 $sql3 = "select * from video where id_prestamo IS NULL";
-$resultado3 = $conexion->query($sql3);
+$resultado3 = $conexion->query($sql3) or die ($sql3);
 print('Nombre del video:');
 print('<select name="videos">');
 for ($i = 0; $i<$resultado3->num_rows;$i++){
